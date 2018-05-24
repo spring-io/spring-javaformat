47ec6d3c6b3bf662e9a54f28fa0a663e389d93cb
Phillip Webb <pwebb@pivotal.io>
2018-05-24 12:02:06 -0700

    Add eclipse update site support

---
+++ ci/scripts/publish-eclipse-update-site.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+set -e
+
+source $(dirname $0)/common.sh
+
+buildName=$( cat artifactory-repo/build-info.json | jq -r '.buildInfo.name' )
+buildNumber=$( cat artifactory-repo/build-info.json | jq -r '.buildInfo.number' )
+groupId=$( cat artifactory-repo/build-info.json | jq -r '.buildInfo.modules[0].id' | sed 's/\(.*\):.*:.*/\1/' )
+version=$( cat artifactory-repo/build-info.json | jq -r '.buildInfo.modules[0].id' | sed 's/.*:.*:\(.*\)/\1/' )
+
+echo "Publishing ${buildName}/${buildNumber} to Eclipse Update Site"
+curl \
+		-s \
+		--connect-timeout 240 \
+		--max-time 2700 \
+		-u ${BINTRAY_USERNAME}:${BINTRAY_PASSWORD} \
+		-f \
+		-X PUT \
+		-T "artifactory-repo/io/spring/javaformat/io.spring.javaformat.eclipse.site/${version}/io.spring.javaformat.eclipse.site-${version}.zip" \
+		"https://api.bintray.com/content/spring/javaformat-eclipse/update-site/${version}/${version}/site.zip?explode=1&publish=1" > /dev/null || { echo "Failed to publish" >&2; exit 1; }
+
+releasedVersions=$( curl -f -X GET https://api.bintray.com/packages/spring/javaformat-eclipse/update-site | jq -r '.versions[]' )
+respositories=""
+while read -r releasedVersion; do
+	respositories="${respositories}<repository><url>https://dl.bintray.com/spring/javaformat-eclipse/${releasedVersion}</url><layout>p2</layout></repository>"
+done <<< "${releasedVersions}"
+
+pushd git-repo > /dev/null
+sed "s|##respositories##|${respositories}|" ci/scripts/publish-eclipse-update-site-pom-template.xml > publish-eclipse-update-site-pom.xml
+run_maven -f publish-eclipse-update-site-pom.xml clean package || { echo "Failed to publish" >&2; exit 1; }
+
+curl \
+		-s \
+		--connect-timeout 240 \
+		--max-time 2700 \
+		-u ${BINTRAY_USERNAME}:${BINTRAY_PASSWORD} \
+		-f \
+		-X PUT \
+		-T "target/repository/content.jar" \
+		"https://api.bintray.com/content/spring/javaformat-eclipse/content.jar" > /dev/null || { echo "Failed to publish" >&2; exit 1; }
+
+curl \
+		-s \
+		--connect-timeout 240 \
+		--max-time 2700 \
+		-u ${BINTRAY_USERNAME}:${BINTRAY_PASSWORD} \
+		-f \
+		-X PUT \
+		-T "target/repository/artifacts.jar" \
+		"https://api.bintray.com/content/spring/javaformat-eclipse/artifacts.jar" > /dev/null || { echo "Failed to publish" >&2; exit 1; }
+
+popd > /dev/null
+
+echo "Publish complete"
