95e4b8e4173cd53975c96c0ea5fd8e0512ec393d
Phillip Webb <pwebb@pivotal.io>
2018-06-19 16:28:15 -0700

    Add check for javadoc case
    Check that `@Param` `@Throws` and `@Return` all start with lowercase.
    
    Closes gh-38

---
+++ spring-javaformat/spring-javaformat-checkstyle/src/main/java/io/spring/javaformat/checkstyle/check/JavadocCheck.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2017-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.spring.javaformat.checkstyle.check;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.puppycrawl.tools.checkstyle.api.AbstractCheck;
+import com.puppycrawl.tools.checkstyle.api.DetailAST;
+import com.puppycrawl.tools.checkstyle.api.TextBlock;
+import com.puppycrawl.tools.checkstyle.api.TokenTypes;
+import com.puppycrawl.tools.checkstyle.utils.CommonUtils;
+
+/**
+ * Checks that the javadoc comments follow Spring conventions.
+ *
+ * @author Phillip Webb
+ */
+public class JavadocCheck extends AbstractCheck {
+
+	private static final Pattern[] PATTERNS = {
+			CommonUtils.createPattern("@param\\s+\\S+\\s+(.*)"),
+			CommonUtils.createPattern("@throws\\s+\\S+\\s+(.*)"),
+			CommonUtils.createPattern("@return\\s+(.*)") };
+
+	@Override
+	public int[] getDefaultTokens() {
+		return new int[] { TokenTypes.INTERFACE_DEF, TokenTypes.CLASS_DEF,
+				TokenTypes.ENUM_DEF, TokenTypes.ANNOTATION_DEF, TokenTypes.METHOD_DEF,
+				TokenTypes.CTOR_DEF };
+	}
+
+	@Override
+	public int[] getAcceptableTokens() {
+		return new int[] { TokenTypes.INTERFACE_DEF, TokenTypes.CLASS_DEF,
+				TokenTypes.ENUM_DEF, TokenTypes.ANNOTATION_DEF, TokenTypes.METHOD_DEF,
+				TokenTypes.CTOR_DEF, TokenTypes.ENUM_CONSTANT_DEF,
+				TokenTypes.ANNOTATION_FIELD_DEF };
+	}
+
+	@Override
+	public int[] getRequiredTokens() {
+		return CommonUtils.EMPTY_INT_ARRAY;
+	}
+
+	@Override
+	public void visitToken(DetailAST ast) {
+		int lineNumber = ast.getLineNo();
+		TextBlock javadoc = getFileContents().getJavadocBefore(lineNumber);
+		if (javadoc != null) {
+			checkParamTags(javadoc);
+		}
+	}
+
+	private void checkParamTags(TextBlock javadoc) {
+		String[] text = javadoc.getText();
+		for (int i = 0; i < text.length; i++) {
+			for (Pattern pattern : PATTERNS) {
+				Matcher matcher = pattern.matcher(text[i]);
+				if (matcher.find()) {
+					String description = matcher.group(1).trim();
+					if (startsWithUppercase(description)) {
+						log(javadoc.getStartLineNo() + i,
+								text[i].length() - description.length(),
+								"javadoc.badCase");
+					}
+				}
+			}
+		}
+	}
+
+	private boolean startsWithUppercase(String description) {
+		return description.length() > 0 && Character.isUpperCase(description.charAt(0));
+	}
+
+}
---
+++ spring-javaformat/spring-javaformat-checkstyle/src/test/resources/source/JavadocValid.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2017-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Javadoc with a bad author tag.
+ *
+ * @param <T> this is a valid param
+ * @author Phillip Webb
+ */
+public class JavadocValid<T> {
+
+	/**
+	 * Do something.
+	 * @param something a lovely thing
+	 */
+	public void test(String something) {
+	}
+
+	/**
+	 * Do something else.
+	 * @param something a loveley thing. Even if we've got some additional desc.
+	 */
+	public void test2(String something) {
+	}
+
+	/**
+	 * Do something else.
+	 * @param something a loveley thing that goes on a bit and causes us to wrap at end.
+	 * Even if we've got some additional desc.
+	 * @return the thing
+	 * @throws RuntimeException on the error
+	 */
+	public String test3(String something) throw RuntimeException {
+	}
+
+}
