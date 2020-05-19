5fa277b8cdea03dff9c5ce8ecdd21449ed49b064
Phillip Webb <pwebb@pivotal.io>
2020-05-19 09:44:53 -0700

    Check for o_O as exception message
    Closes gh-189

--- spring-javaformat/spring-javaformat-checkstyle/src/main/java/io/spring/javaformat/checkstyle/check/SpringCatchCheck.java
+++ spring-javaformat/spring-javaformat-checkstyle/src/main/java/io/spring/javaformat/checkstyle/check/SpringCatchCheck.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2019 the original author or authors.
+ * Copyright 2017-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -59,6 +59,9 @@ public class SpringCatchCheck extends AbstractSpringCheck {
 		String text = ast.getText();
 		if (text.length() == 1) {
 			log(ast.getLineNo(), ast.getColumnNo(), "catch.singleLetter");
+		}
+		if (text.toLowerCase().equals("o_o")) {
+			log(ast.getLineNo(), ast.getColumnNo(), "catch.wideEye");
 		}
 	}
 
