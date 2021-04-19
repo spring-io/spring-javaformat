235279e134aee809fbc1c315cf70b10af5888182
Phillip Webb <pwebb@vmware.com>
2021-04-19 11:13:34 -0700

    Migrate update site to repo.spring.io
    Closes gh-263

--- ci/scripts/publish-eclipse-update-site.sh
+++ ci/scripts/publish-eclipse-update-site.sh
@@ -9,25 +9,23 @@ groupId=$( cat artifactory-repo/build-info.json | jq -r '.buildInfo.modules[0].i
 version=$( cat artifactory-repo/build-info.json | jq -r '.buildInfo.modules[0].id' | sed 's/.*:.*:\(.*\)/\1/' )
 
 echo "Publishing ${buildName}/${buildNumber} to Eclipse Update Site"
-# We need to push twice for some reason otherwise we get out of date versions
-for i in {1..2}; do
-	curl \
-		-s \
-		--connect-timeout 240 \
-		--max-time 2700 \
-		-u ${BINTRAY_USERNAME}:${BINTRAY_API_KEY} \
-		-f \
-		-X PUT \
-		-T "artifactory-repo/io/spring/javaformat/io.spring.javaformat.eclipse.site/${version}/io.spring.javaformat.eclipse.site-${version}.zip" \
-		"https://api.bintray.com/content/spring/javaformat-eclipse/update-site/${version}/${version}/site.zip?explode=1&publish=1" > /dev/null || { echo "Failed to publish" >&2; exit 1; }
-	releasedVersions=$( curl -f -X GET https://api.bintray.com/packages/spring/javaformat-eclipse/update-site | jq -r '.versions[]' )
-	sleep 30
-done
+curl \
+	-s \
+	--connect-timeout 240 \
+	--max-time 2700 \
+	-u ${ARTIFACTORY_USERNAME}:${ARTIFACTORY_PASSWORD} \
+	-f \
+	-H "X-Explode-Archive: true"
+	-X PUT \
+	-T "artifactory-repo/io/spring/javaformat/io.spring.javaformat.eclipse.site/${version}/io.spring.javaformat.eclipse.site-${version}.zip" \
+	"https://repo.spring.io/javaformat-eclipse-update-site/${version}" > /dev/null || { echo "Failed to publish" >&2; exit 1; }
 
-respositories=""
+releasedVersions=$( curl -s -f -X GET https://repo.spring.io/api/storage/javaformat-eclipse-update-site | jq -r '.children[] | .uri' | cut -c 2- | grep '\d.*' | sort -V )
+
+repositories=""
 while read -r releasedVersion; do
 	echo "Adding repository for ${releasedVersion}"
-	respositories="${respositories}<repository><url>https://dl.bintray.com/spring/javaformat-eclipse/${releasedVersion}</url><layout>p2</layout></repository>"
+	repositories="${repositories}<repository><url>https://repo.spring.io/javaformat-eclipse-update-site/${releasedVersion}</url><layout>p2</layout></repository>"
 done <<< "${releasedVersions}"
 
 pushd git-repo > /dev/null
@@ -38,21 +36,21 @@ curl \
 		-s \
 		--connect-timeout 240 \
 		--max-time 2700 \
-		-u ${BINTRAY_USERNAME}:${BINTRAY_API_KEY} \
+		-u ${ARTIFACTORY_USERNAME}:${ARTIFACTORY_PASSWORD} \
 		-f \
 		-X PUT \
 		-T "target/repository/content.jar" \
-		"https://api.bintray.com/content/spring/javaformat-eclipse/content.jar" > /dev/null || { echo "Failed to publish" >&2; exit 1; }
+		"https://repo.spring.io/javaformat-eclipse-update-site" > /dev/null || { echo "Failed to publish" >&2; exit 1; }
 
 curl \
 		-s \
 		--connect-timeout 240 \
 		--max-time 2700 \
-		-u ${BINTRAY_USERNAME}:${BINTRAY_API_KEY} \
+		-u ${ARTIFACTORY_USERNAME}:${ARTIFACTORY_PASSWORD} \
 		-f \
 		-X PUT \
 		-T "target/repository/artifacts.jar" \
-		"https://api.bintray.com/content/spring/javaformat-eclipse/artifacts.jar" > /dev/null || { echo "Failed to publish" >&2; exit 1; }
+		"https://repo.spring.io/javaformat-eclipse-update-site" > /dev/null || { echo "Failed to publish" >&2; exit 1; }
 
 popd > /dev/null
 
