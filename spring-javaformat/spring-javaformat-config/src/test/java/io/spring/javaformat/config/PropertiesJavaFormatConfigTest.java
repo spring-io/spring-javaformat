fcc6fb2361352c3d0ffc42017d942ca0627a2cfd
Rivaldi <cbot59@gmail.com>
2022-02-03 07:38:22 +0700

    test: add PropertiesJavaFormatConfigTest

---
+++ spring-javaformat/spring-javaformat-config/src/test/java/io/spring/javaformat/config/PropertiesJavaFormatConfigTest.java
@@ -0,0 +1,54 @@
+package io.spring.javaformat.config;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+import java.util.Properties;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+class PropertiesJavaFormatConfigTest {
+
+    private JavaFormatConfig javaFormatConfig;
+
+    @Mock
+    private Properties properties;
+
+    private AutoCloseable closeable;
+
+    @BeforeEach
+    void setUp() {
+        closeable = MockitoAnnotations.openMocks(this);
+        javaFormatConfig = new PropertiesJavaFormatConfig(properties);
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+        closeable.close();
+    }
+
+    @Test
+    void getJavaBaselineWhenNullShouldReturnDefaultJavaBaseline() {
+        when(properties.get(anyString()))
+            .thenReturn(null);
+
+        JavaBaseline actual = javaFormatConfig.getJavaBaseline();
+
+        assertThat(actual).isEqualTo(JavaBaseline.V11);
+    }
+
+    @Test
+    void getJavaBaselineWhenSetAs8ShouldReturnJavaBaselineV8() {
+        when(properties.get(anyString()))
+            .thenReturn(8);
+
+        JavaBaseline actual = javaFormatConfig.getJavaBaseline();
+
+        assertThat(actual).isEqualTo(JavaBaseline.V8);
+    }
+}
