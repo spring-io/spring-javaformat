beae9ecb39859505168b2116d0843335c40d05a3
Andy Wilkinson <wilkinsona@vmware.com>
2022-05-26 11:02:36 +0100

    Upgrade to Checkstyle 9.3
    Closes gh-333

--- spring-javaformat/spring-javaformat-checkstyle/src/test/resources/source/TernaryInIf.java
+++ spring-javaformat/spring-javaformat-checkstyle/src/test/resources/source/TernaryInIf.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2019 the original author or authors.
+ * Copyright 2017-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -24,7 +24,9 @@ public class TernaryInIf {
 	public void test() {
 		boolean a = true;
 		boolean b = false;
-		if ((a != b) ? true : false) {
+		int c = 1;
+		int d = 2;
+		if ((a != b) ? d < c : c >= d) {
 			System.out.println("OK");
 		}
 	}
