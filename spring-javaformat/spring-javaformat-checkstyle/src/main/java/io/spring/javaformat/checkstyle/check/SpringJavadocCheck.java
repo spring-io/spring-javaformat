9610342bc6ed635395d16ca57d3cac8b6a681f8f
Andy Wilkinson <wilkinsona@vmware.com>
2022-10-03 11:14:53 +0100

    Polish

--- spring-javaformat/spring-javaformat-checkstyle/src/main/java/io/spring/javaformat/checkstyle/check/SpringJavadocCheck.java
+++ spring-javaformat/spring-javaformat-checkstyle/src/main/java/io/spring/javaformat/checkstyle/check/SpringJavadocCheck.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2021 the original author or authors.
+ * Copyright 2017-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -228,7 +228,7 @@ public class SpringJavadocCheck extends AbstractSpringCheck {
 	private DetailAST getInterfaceOrAnnotationDef(DetailAST ast) {
 		return findParent(ast, TokenTypes.INTERFACE_DEF, TokenTypes.ANNOTATION_DEF);
 	}
-	
+
 	private DetailAST findParent(DetailAST ast, int... classDefs) {
 		while (ast != null) {
 			for (int classDef: classDefs) {
