fbd0c3365d507ee989c9c0931d43e36ce0da32b2
Phillip Webb <pwebb@vmware.com>
2023-02-16 16:16:36 -0800

    Add WebMvcLinkBuilder as allowed static import
    Add `org.springframework.hateoas.server.mvc.WebMvcLinkBuilder` as an
    allowed static import and remove legacy class reference.
    
    Closes gh-350

--- spring-javaformat/spring-javaformat-checkstyle/src/main/java/io/spring/javaformat/checkstyle/check/SpringAvoidStaticImportCheck.java
+++ spring-javaformat/spring-javaformat-checkstyle/src/main/java/io/spring/javaformat/checkstyle/check/SpringAvoidStaticImportCheck.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2022 the original author or authors.
+ * Copyright 2017-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -55,7 +55,7 @@ public class SpringAvoidStaticImportCheck extends AvoidStaticImportCheck {
 		excludes.add("org.springframework.boot.configurationprocessor.ConfigurationMetadataMatchers.*");
 		excludes.add("org.springframework.boot.configurationprocessor.TestCompiler.*");
 		excludes.add("org.springframework.boot.test.autoconfigure.AutoConfigurationImportedCondition.*");
-		excludes.add("org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo");
+		excludes.add("org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*");
 		excludes.add("org.springframework.restdocs.headers.HeaderDocumentation.*");
 		excludes.add("org.springframework.restdocs.hypermedia.HypermediaDocumentation.*");
 		excludes.add("org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.*");
