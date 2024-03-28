b3329ba407f915f5d04f8132b6db2e0bb94b850f
Andy Wilkinson <andy.wilkinson@broadcom.com>
2024-03-28 13:10:25 +0000

    Stop using legacy syntax for alternative repository

--- ci/scripts/build-project.sh
+++ ci/scripts/build-project.sh
@@ -5,5 +5,5 @@ source $(dirname $0)/common.sh
 repository=$(pwd)/distribution-repository
 
 pushd git-repo > /dev/null
-run_maven clean deploy -U -Dfull -DaltDeploymentRepository=distribution::default::file://${repository}
+run_maven clean deploy -U -Dfull -DaltDeploymentRepository=distribution::file://${repository}
 popd > /dev/null
