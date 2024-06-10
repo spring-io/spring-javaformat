66bf43f70d043452590330c68effdb18efe40e70
Phillip Webb <phil.webb@broadcom.com>
2024-06-10 09:28:28 -0700

    Delete Concourse CI
    Closes gh-411

--- ci/scripts/build-project.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-source $(dirname $0)/common.sh
-repository=$(pwd)/distribution-repository
-
-pushd git-repo > /dev/null
-run_maven clean deploy -U -Dfull -DaltDeploymentRepository=distribution::file://${repository}
-popd > /dev/null
--- ci/scripts/common.sh
+++
@@ -1,9 +0,0 @@
-source /opt/concourse-java.sh
-
-setup_symlinks
-
-if [[ -n $DOCKER_HUB_USERNAME ]]; then
-	docker login -u $DOCKER_HUB_USERNAME -p $DOCKER_HUB_PASSWORD
-fi
-
-cleanup_maven_repo "io.spring.javaformat"
